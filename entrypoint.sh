a8fdb3e9730191809e7cee71bdaf0202466153dc
Carlos Vega Meyer <carlosvega09@gmail.com>
2018-07-29 23:38:25 -0300

    Makes basic auth an optional feature (by not specifying BASIC_AUTH_* env vars)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
+[ "" == "$PORT" ] && echo 'No $PORT specified' && exit 1
+[ "" == "$UPSTREAM_URL" ] && echo 'No $UPSTREAM_URL specified' && exit 1
+
 [ "" != "$BASIC_AUTH_USERNAME" ] && [ "" != "$BASIC_AUTH_PASSWORD" ] \
   && htpasswd -c -b /etc/nginx/.htpasswd $BASIC_AUTH_USERNAME $BASIC_AUTH_PASSWORD
 
 [ "" != "$HTPASSWD_FILE_URL" ] \
   && wget -O /etc/nginx/.htpasswd $HTPASSWD_FILE_URL
 
-[ ! -s /etc/nginx/.htpasswd ] && echo 'No credentials provided' && exit 1
-
-envsubst </etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$UPSTREAM_URL $PORT' </etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
 
